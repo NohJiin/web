fbcf9a2c7fdab3b650d7c31e2f11c000af4db3bf
NohJiin <93262813+NohJiin@users.noreply.github.com>
2023-02-02 17:36:52 +0900

    day04 project

---
+++ day04/out.js
@@ -0,0 +1,19 @@
+function laundury() {
+    console.log("1. 먼지를 털자.")
+    console.log("2. 물에 집어 넣는다.")
+    console.log("3. 비누를 바른다.")
+    console.log("4. 오물조물 비빈다.")
+    console.log("5. 물에 여러번 헹군다.")
+    console.log("6. 짠다.")
+    console.log("7. 넌다.")
+}
+
+function clean() {
+    document.write("1. 마음의 준비를 한다. <br>")
+    document.write("2. 청소할 곳을 둘러본다. <br>")
+    document.write("3. 어질러진 것을 정리한다. <br>")
+    document.write("4. 청소기를 돌린다. <br>")
+    document.write("5. 청소한 곳을 닦는다. <br>")
+    document.write("6. 쓰레기를 정리한다. <br>")
+    document.write("7. 쓰레기를 배출한다. <br>")
+}
---
+++ day04/out2.js
@@ -0,0 +1,7 @@
+function trip() {
+    document.write("여행을 떠나요!<br>")
+}
+
+function rest() {
+    document.write("휴식이 필요해요!<br>")
+}
