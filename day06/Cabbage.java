c3d135b9050adebae12ed4630b727866c6cb4ff7
NohJiin <93262813+NohJiin@users.noreply.github.com>
2023-02-06 18:03:41 +0900

    day06 project

---
+++ day06/Cabbage.java
@@ -0,0 +1,10 @@
+package 채소;
+
+public class Cabbage {
+
+	public static void main(String[] args) {
+		System.out.print("이것은 배추입니다.");
+
+	}
+
+}
---
+++ day06/Hello.java
@@ -0,0 +1,13 @@
+package 변수;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// 글자 여러개를 칠때는 반드시 ""를 써야함.
+		// 다 한줄을 쳤으면 그 줄이 끝났다고 ;를 써주어야함
+		System.out.println("안녕하세요 저는 이클립스랍니다.");
+		System.out.println("안녕하세요 저는 이클립스랍니다.");
+		// ctrl + F11
+		// Syso, Sysout + ctrl + spacebar : 자동완성
+	}
+}
